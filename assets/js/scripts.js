4fbc9255628152f52972dd9708b931f2d8097d0f
H.Y.C <chenghsienyu@aim.com>
2022-02-06 19:48:58 +0100

    Updated from Publii

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,13 @@
 // This is your main script file
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.left = "0";
+  } else {
+    document.getElementById("navbar").style.left = "-30%";
+  }
+  prevScrollpos = currentScrollPos;
+}

44e374cea8472c3c3c9467b27b0f2f7fb9af69da
H.Y.C <chenghsienyu@aim.com>
2022-04-24 21:17:07 +0800

    Updated from Publii

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,24 +2,25 @@
 
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
-var getBrowserWidth = function(){
-  if(window.innerWidth < 768){
-      // Extra Small Device
-      return "xs";
-  } else if(window.innerWidth < 991){
-      // Small Device
-      return "sm"
-  } else if(window.innerWidth < 1199){
-      // Medium Device
-      return "md"
+
+var getBrowserWidth = function () {
+  if (window.innerWidth < 768) {
+    // Extra Small Device
+    return "xs";
+  } else if (window.innerWidth < 991) {
+    // Small Device
+    return "sm"
+  } else if (window.innerWidth < 1199) {
+    // Medium Device
+    return "md"
   } else {
-      // Large Device
-      return "lg"
+    // Large Device
+    return "lg"
   }
 };
 var device = getBrowserWidth();
 
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   console.log(window.innerWidth);
   if (device == "xs") {
@@ -29,12 +30,37 @@ window.onscroll = function() {
     } else {
       document.getElementById("navbar").style.left = "-90%";
     }
-  }else{
+  } else {
     if (prevScrollpos > currentScrollPos) {
       document.getElementById("navbar").style.left = "0";
     } else {
       document.getElementById("navbar").style.left = "-30%";
+    }
+    prevScrollpos = currentScrollPos;
   }
-  prevScrollpos = currentScrollPos;
-}
 };
+
+function handleStart(evt) {
+  evt.preventDefault();
+  log('touchstart.');
+  const el = document.getElementById('canvas');
+  const touches = evt.changedTouches;
+
+  for (let i = 0; i < touches.length; i++) {
+    log(`touchstart: ${i}.`);
+    document.getElementById("navbar").style.left = "-90%";
+
+  }
+}
+
+function startup() {
+  const el = document.getElementById('navbar');
+  el.addEventListener('touchstart', handleStart);
+  el.addEventListener('touchend', handleEnd);
+  el.addEventListener('touchcancel', handleCancel);
+  el.addEventListener('touchmove', handleMove);
+  log('Initialized.');
+}
+
+document.addEventListener("DOMContentLoaded", startup);
+handleStart();

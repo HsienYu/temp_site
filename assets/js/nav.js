2d1c17b13d96043fba7b3dda6df49cdd079529ce
H.Y.C <chenghsienyu@aim.com>
2023-01-09 13:16:58 +0100

    Updated from Publii

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,1 +1,10 @@
 $('ul').addClass('menu');
+$(".menu ul.submenu.level-2 li a").each(function (index, item) {
+    var title = $(item).text();
+    var isNumber = $.isNumeric(title);
+    if (isNumber) {
+        $(item).append('⬅︎⬅︎⬅︎');
+    } else if (title == 'Works') {
+        $(item).append('⬅︎⬅︎⬅︎');
+    }
+});

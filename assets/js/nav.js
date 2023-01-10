642f26e2e93a7d8ea7ca1893fcc91befcb83d113
H.Y.C <chenghsienyu@aim.com>
2023-01-10 18:06:11 +0100

    Updated from Publii

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,10 +1,24 @@
 $('ul').addClass('menu');
 $(".menu ul.submenu.level-2 li a").each(function (index, item) {
     var title = $(item).text();
+    var distance = $(item).offset().top;
     var isNumber = $.isNumeric(title);
     if (isNumber) {
         $(item).append('⬅︎⬅︎⬅︎');
         $(item).addClass('sticky');
+        $(window).scroll(function () {
+            if ($(this).scrollTop() >= distance) {
+                // console.log('is in top');
+                $(item).css({
+                    fontSize: "36px"
+                })
+            } else {
+                // console.log('is not in top');
+                $(item).css({
+                    fontSize: "20px"
+                })
+            }
+        });
     } else if (title == 'Works') {
         $(item).append('⬅︎⬅︎⬅︎');
     }
